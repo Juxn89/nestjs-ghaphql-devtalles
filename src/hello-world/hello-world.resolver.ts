8ade565c1ed14ad34162bca27ac9c8a1bf1161eb
Juan Gómez <gomezj01@stepstone.com>
2023-11-21 09:45:54 -0600

    Nueva Query

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,19 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-    @Query( () => String )
+    @Query( () => String, { name: 'ShowSmileFace', description: 'Returns a smile face' } )
     helloWorl(): string {
         return ':)'
+    }
+
+    @Query( () => Float, { name: 'RandomNumber' } )
+    getRandoNumber(): number {
+        return Math.random() * 100;
+    }
+
+    @Query( () => Int, { name: 'RandomNumberFromZeroTo' } )
+    getRandoFromZeroTo(): number {
+        return Math.floor(Math.random() * 10)
     }
 }
