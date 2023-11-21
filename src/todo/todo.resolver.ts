3a59f29b43ce0403e5d8cfd1c79e7a412a167728
Juan Gómez <gomezj01@stepstone.com>
2023-11-21 11:46:36 -0600

    Retornar lista de todo y todo por ID

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { Todo } from './entity/todo.entity';
 import { TodoService } from './todo.service';
 
@@ -11,5 +11,12 @@ export class TodoResolver {
 	@Query( () => [Todo], { name: 'todos' } )
 	findAll(): Todo[] {
 		return this.todoService.findAll()
+	}
+
+	@Query( () => Todo, { name: 'todo' } )
+	findOne(
+		@Args( 'id', { type: () => Int } ) id: number
+	): Todo {
+		return this.todoService.findOne(id);
 	}
 }
